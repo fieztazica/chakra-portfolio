87214a970decf37966f30d7718287b18da293eb7
Fiezt <dat09t@gmail.com>
2022-10-29 17:55:47 +0700

    wwtf

--- components/projects/GitHubRepo.jsx
+++ components/projects/GitHubRepo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { data } from "../../data/github/fake";
 import { getOwnerRepos } from "../../lib/github";
 import SectionHeading from "../shared/SectionHeading";
-import RepoCard from "./repoCard";
+import RepoCard from "./RepoCard";
 
 const array = [data];
 
@@ -27,7 +27,7 @@ function GitHubRepo() {
   React.useEffect(() => {
     fetchRepos();
   }, []);
-  
+
   return (
     <>
       <SectionHeading mb={4}>
